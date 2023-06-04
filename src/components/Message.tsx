5197cbdfd2bfe2c9fcc32ba1225e32d58fec10c0
Charles Hals <hals.charles@gmail.com>
2023-06-04 18:29:25 +0200

    Update

---
+++ src/components/Message.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MessageInt } from '../model';
+type Props = {
+    mess:MessageInt,
+    messData: MessageInt[],
+    setMessData:React.Dispatch<React.SetStateAction<MessageInt[]>>
+}
+const Message = ({mess, messData,setMessData}:Props) => {
+    const dateFormater = (date:number) => {
+        return new Date(date).toLocaleDateString("fr-FR")
+    }
+    const handleDelete = () => {
+         setMessData(messData.filter((el) => el.id !== mess.id))
+    }
+    return (
+        <div className='message-container'>  
+            <p>{mess.message}</p>
+            <h5>{dateFormater(mess.date)}</h5>
+            <span id="delete" onClick={handleDelete}>&#10008;</span>
+        </div>
+    );
+};
+export default Message;
